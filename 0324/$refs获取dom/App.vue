ca46364f6c9c0528583323706d9e17d49f3c4a96
zrx123123 <3338336047@qq.com>
2025-03-24 23:08:35 +0800

    2025-03-24代码

---
+++ 0324/$refs获取dom/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <div class="base-chart-box">
+      这是一个捣乱的盒子
+    </div>
+    <BaseChart></BaseChart>
+  </div>
+</template>
+
+<script>
+import BaseChart from './components/BaseChart.vue'
+export default {
+  data(){
+    return{
+      
+    }
+  },
+  components:{
+    BaseChart
+  }
+}
+</script>
+
+<style>
+.base-chart-box {
+  width: 300px;
+  height: 200px;
+}
+</style>
