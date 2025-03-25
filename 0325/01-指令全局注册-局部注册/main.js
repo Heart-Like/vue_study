1c9b3134c56e229bd02edd5cc47eeac108a1866e
zrx123123 <3338336047@qq.com>
2025-03-25 23:20:22 +0800

    2025-03-25代码

---
+++ 0325/01-指令全局注册-局部注册/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+// 全局注册指令
+Vue.directive('focus', {
+  inserted(el) {
+    el.focus()
+  }
+})
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
