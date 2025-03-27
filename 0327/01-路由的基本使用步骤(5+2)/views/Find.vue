8d1d714c3c882d5b068276062cf97f628929ba1e
zrx123123 <3338336047@qq.com>
2025-03-27 22:41:53 +0800

    2025-03-27代码

---
+++ 0327/01-路由的基本使用步骤(5+2)/views/Find.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>发现音乐</p>
+    <p>发现音乐</p>
+    <p>发现音乐</p>
+    <p>发现音乐</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // name要由多个单词组成
+  name: 'FindMusic'
+}
+</script>
+
+<style>
+
+</style>
