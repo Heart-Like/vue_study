9ed4b0076e161e943d6c57e3f608ea382da0abef
zrx123123 <3338336047@qq.com>
2025-04-13 21:07:45 +0800

    2025-04-13代码

---
+++ 0413/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="box"></div>
+    <!-- 测试代码 -->
+     <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+
+    <van-switch v-model="checked" />
+    <van-rate v-model="value" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default ({
+  data () {
+    return {
+      checked: true,
+      value: 3
+    }
+  }
+})
+</script>
+
+<style lang="less">
+.box {
+  width: 300px;
+  height: 300px;
+  background-color: pink;
+}
+</style>
---
+++ 0413/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Layout from '@/views/layout'
+import Search from '@/views/search'
+import SearchList from '@/views/search/list'
+import ProDetail from '@/views/prodetail'
+import Login from '@/views/login'
+import Pay from '@/views/pay'
+import MyOrder from '@/views/myorder'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/',
+      component: Layout
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/searchlist',
+      component: SearchList
+    },
+    {
+      path: '/prodetail/:id',
+      component: ProDetail
+    },
+    {
+      path: '/pay',
+      component: Pay
+    },
+    {
+      path: '/myorder',
+      component: MyOrder
+    }
+  ]
+})
+
+export default router
