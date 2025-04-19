a43ea2647b6b13609fe6327b0ec561df6bbc1e94
zrx123123 <3338336047@qq.com>
2025-04-19 22:52:17 +0800

    2025-04-19代码

---
+++ 0419/src/utils/vant-ui.js
@@ -0,0 +1,16 @@
+// 按需导入
+import Vue from 'vue'
+import { Toast, Button, Switch, Rate, Tabbar, TabbarItem, NavBar, Search, Swipe, SwipeItem, Grid, GridItem } from 'vant'
+
+Vue.use(GridItem)
+Vue.use(Search)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Grid)
+Vue.use(Toast)
+Vue.use(NavBar)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
+Vue.use(Button)
+Vue.use(Switch)
+Vue.use(Rate)
