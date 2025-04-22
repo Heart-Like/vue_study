3c62dcb1ad824dda50ccb57871f3b64b24925d07
zrx123123 <3338336047@qq.com>
2025-04-22 20:57:29 +0800

    2025-04-22代码

---
+++ 0422/src/store/modules/user.js
@@ -0,0 +1,17 @@
+import { getInfo, setInfo } from '@/utils/storage'
+export default {
+  namespaced: true,
+  state () {
+    return {
+      userInfo: getInfo()
+    }
+  },
+  mutations: {
+    // 所有mutations的第一个参数都是state
+    setUserInfo (state, obj) {
+      state.userInfo = obj
+      setInfo(obj)
+    }
+  },
+  actions: {}
+}
