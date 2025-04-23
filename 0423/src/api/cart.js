ed308de88c605d053601803fb66903f8a921869e
zrx123123 <3338336047@qq.com>
2025-04-23 20:45:49 +0800

    2025-04-23代码

---
+++ 0423/src/api/cart.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+// 加入购物车
+export const addCart = (goodsId, goodsNum, goodsSkuId) => {
+  return request.post('/cart/add', {
+    goodsId,
+    goodsNum,
+    goodsSkuId
+  })
+}
+
+// 获取购物车列表数据
+export const getCartList = () => {
+  return request.get('/cart/list')
+}
+
+// 更新购物车商品数量
+export const changeCount = (goodsId, goodsNum, goodsSkuId) => {
+  return request.post('/cart/update', {
+    goodsId,
+    goodsNum,
+    goodsSkuId
+  })
+}
